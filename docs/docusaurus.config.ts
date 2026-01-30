71429b2c7fe2c30bc9899045d8cc745a7a4b4c40
ShodaiMomiyama <mmymshd52@gmail.com>
2026-01-31 02:22:51 +0900

    Update production URL in docusaurus.config.ts to point to the new documentation site

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://formix.netlify.app',
+  url: 'https://formix-docs.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: '/',
 
